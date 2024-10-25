5b25134fe43ffd443dcaae8d8b7600698f0e726f
vk <ftek1.3@gmail.com>
2024-10-25 20:30:40 +0300

    Add project files.

---
+++ FactoryLib/FactoryMethod.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryLib
+{
+    public interface IAnimal
+    {
+        string Speak();
+    }
+
+    public class Cat : IAnimal
+    {
+        public string Speak()
+        {
+            return "Meow";
+        }
+    }
+
+    public class Dog : IAnimal
+    {
+        public string Speak()
+        {
+            return "Woof";
+        }
+    }
+
+    public abstract class AnimalCreator
+    {
+        public abstract IAnimal CreateAnimal();
+
+        public string AnimalInfo()
+        {
+            var animal = CreateAnimal();
+            return $"This animal says: {animal.Speak()}";
+        }
+    }
+
+    public class CatCreator : AnimalCreator
+    {
+        public override IAnimal CreateAnimal()
+        {
+            return new Cat();
+        }
+    }
+
+    public class DogCreator : AnimalCreator
+    {
+        public override IAnimal CreateAnimal()
+        {
+            return new Dog();
+        }
+    }
+}
